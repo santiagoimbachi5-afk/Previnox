9a8a99cc84fe4fbaf198e7261d0a9bbe7ceb8f62
santiagoimbachi5-afk <santiagoimbachi5@gmail.com>
2026-06-22 23:36:57 -0500

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,9 @@
+document.getElementById("boton-panico").addEventListener("click", function() {
+  // Funcion del boton de panico
+  activarBotonPanico();
+});
+
+function activarBotonPanico() {
+  alert("¡Ayuda en camino! Se ha activado el boton de panico.");
+  // Aqui puedes agregar la logica real: enviar alerta, ubicacion, etc.
+}

67953cbd835f8ecf350c18f2726eb03a7df1aca9
santiagoimbachi5-afk <santiagoimbachi5@gmail.com>
2026-08-17 18:23:20 -0500

    Update login-entidaes_salud.js

--- Home/Js/login-entidaes_salud.js
+++ Home/Js/login-entidaes_salud.js
@@ -1,6 +1,6 @@
 function ingresar(e) {
     
       e.preventDefault();
-      alert('¡Acceso concedido como Profesional de Salud!');
-      window.location.href = 'dashboard-entidaes_salud';
+      alert('¡Acceso concedido como Entidad de Salud!');
+      window.location.href = 'dashboard-entidades_salud.html';
 }
